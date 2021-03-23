0dbd6ad7e7170b6256923c0f7bd3a856ff477812
Kyle Culp <me@kyleculp.com>
2021-03-23 22:33:08 +0000

    automated push

--- scripts/create-secret-cert.sh
+++ scripts/create-secret-cert.sh
@@ -5,6 +5,6 @@ if [ $# -eq 0 ]
 fi
 # /scripts/create-secret-cert.sh asicforecast-tls priv/asicforecast/cert.pem priv/asicforecast/key.pem traefik
 kubectl create secret tls $1 --cert $2 --key $3 -n $4 --dry-run=client -o json | \
-  kubeseal --cluster k3d-homecluster --controller-name sealed-secrets -o yaml > homelab/kube-system/certs/$1.yaml
+  kubeseal  -o yaml > homelab/traefik/certs/$1.yaml
 
-echo "'homelab/kube-system/certs-o/$1.yaml' is safe to upload to github"
+echo "'homelab/traefik/certs-o/$1.yaml' is safe to upload to github"
--- scripts/create-secret.sh
+++ scripts/create-secret.sh
@@ -5,6 +5,6 @@ if [ $# -eq 0 ]
 fi
 
 kubectl create secret generic $1 --dry-run=client --from-env-file=$2 -n $3 -o json | \
-  kubeseal --cluster k3d-homecluster --controller-name sealed-secrets -o yaml > homelab/kube-system/secrets/$1.yaml
+  kubeseal -o yaml > priv/build/$1-secret.yaml
 
-echo "'homelab/kube-system/secrets/$1.yaml' is safe to upload to github"
+echo "'priv/build/$1-secret.yaml' is safe to upload to github"
